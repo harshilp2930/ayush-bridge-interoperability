be1441d5b89e0b56e6cb51dea4ea2472a1c1ddef
harshilp2930 <harshilp2930@gmail.com>
2025-12-13 10:43:49 +0530

    Redirect login to admin page

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    
+    path('accounts/login/', admin.site.login),
     # Documentation URLs
     path('api-docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
